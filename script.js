1038a5baf3a399a3dc6c6f23a2bf565ccee8dbd6
intwaza <iintwazabelyse@gmail.com>
2021-03-24 20:59:42 +0300

    trial

---
+++ script.js
@@ -0,0 +1,22 @@
+function getgrade(marks) {
+    var marks = 84;
+    if (marks>=80){
+    console.log(grade="A")
+}
+else if(marks>=70 && marks<=79){
+    console.log(grade="B")
+}
+else if(marks>=60 && marks<=69){
+    console.log(grade="C")
+}
+else if(marks>=50 && marks<=59){
+    console.log(grade="D")
+}
+else if(marks>=0 && marks<=49){
+    console.log(grade="E")
+}
+else{
+    console.log("F")
+}
+}
+getgrade();
